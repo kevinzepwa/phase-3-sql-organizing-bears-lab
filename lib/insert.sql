ddb46032658a88059be9794ae41259c435476512
kevinzepwa <zepwakevin8@gmail.com>
2022-11-07 10:44:31 +0300

    complete

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)  VALUES ("Kevin", 5,"M", "brown", "sweet", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive)  VALUES ("Tom", 6,"F", "straw", "sweet", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive)  VALUES ("Lydia", 5,"F", "violet", "bad", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive)  VALUES ("Marcus", 5,"M", "brown", "sweet", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive)  VALUES ("Lizabeth", 5,"F", "pink", "sweet", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive)  VALUES ("Irene", 5,"F", "blue", "sweet", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive)  VALUES ("Vivian", 5,"F", "brown", "bad", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive)  VALUES (NULL, 5,"F", "black", "sweet", 1);
